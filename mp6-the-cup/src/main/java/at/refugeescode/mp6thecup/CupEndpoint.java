b28dd73f0ee5dba7a96db3845fc4c0d4daf3b92e
nana80802 <miss_nisreen2000@yahoo.com>
2018-04-01 15:23:18 +0200

    finished the cup part

--- mp6-the-cup/src/main/java/at/refugeescode/mp6thecup/CupEndpoint.java
+++ mp6-the-cup/src/main/java/at/refugeescode/mp6thecup/CupEndpoint.java
@@ -1,11 +1,24 @@
 package at.refugeescode.mp6thecup;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/coin")
 public class CupEndpoint {
     private Boolean coin;
 
+    @DeleteMapping
+    void releaseCup(){
+        coin = false;
+    }
+
+    @PutMapping
+    void putCoin(){
+        coin = true;
+    }
+
+    @GetMapping
+    Boolean getCoin(){
+        return coin;
+    }
 }
